fa2d51e53d61dad1a2c067b2f0dd1bef9b6ab8e4
Isabelle <isabellefotograf@gmail.com>
2024-10-15 17:02:28 +0200

    changed the h1 in HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import SearchBar from '../components/SearchBar';
-import MovieList from '../components/MovieList';
+import React from "react";
+import SearchBar from "../components/SearchBar";
+import MovieList from "../components/MovieList";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center my-4">Welcome to Hemmakvälls Movie Library</h1>
+      <h1 className="text-4xl font-bold text-center my-4">
+        Welcome to the Movie App
+      </h1>
       <SearchBar />
       <MovieList />
     </div>
