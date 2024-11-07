cadf93addb532d4961742505157f0bbe7054b716
Isabelle <isabellefotograf@gmail.com>
2024-11-07 15:12:59 +0100

    changed the movies id route

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/movie/:id" element={<MovieDetailsPage />} />
+        <Route path="/movies/:id" element={<MovieDetailsPage />} />
       </Routes>
     </Router>
   );
