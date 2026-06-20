a6017909a4d8c2ee9428fd33c0cdd7e98dc927a6
Isabelle Löfgren <104595550+IsabelleLof@users.noreply.github.com>
2026-06-20 21:33:03 +0200

    Added Github Workflows to the Cypresstests

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = ({ movies }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} /> // Skicka movie som en prop till MovieCard
+        <MovieCard key={movie.id} movie={movie}  /> // Skicka movie som en prop till MovieCard
       ))}
     </div>
   );
